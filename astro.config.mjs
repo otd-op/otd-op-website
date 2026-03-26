a3b15787af3433a0cd5d67e5ba6678499f6cd936
Amir Azizafshari <55924754+amir002001@users.noreply.github.com>
2026-03-26 19:24:27 -0400

    rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://otd-op.org',
   adapter: cloudflare(),
   output: 'static',
 });
