60879954a93767de110104845104e0424144c168
Amir Azizafshari <55924754+amir002001@users.noreply.github.com>
2026-03-26 23:25:53 -0400

    feat: add sitemap integration and placeholder pages for documents and join

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://otd-op.org',
   adapter: cloudflare(),
   output: 'static',
+  integrations: [sitemap()],
 });
